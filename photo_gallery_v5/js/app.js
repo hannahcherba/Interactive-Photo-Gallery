e43e78d11d97c1aa2fc2637ee0a3628ab3420c81
Hannah Cherba <hannahpond@yahoo.com>
2023-03-21 13:21:27 -0700

    Added baguetteBox files

--- photo_gallery_v5/js/app.js
+++ photo_gallery_v5/js/app.js
@@ -2,16 +2,16 @@ const search = new Filter('search', 'data-caption');
 
 baguetteBox.run('.gallery', {
     captions: true,
+    buttons: 'auto',
     fullScreen: false,
     noScrollbars: false,
     titleTag: true,
     async: false,
     preload: 2,
-    animation: 'fadeIn',
+    // animation: 'fadeIn',
     afterShow: function() {},
     afterHide: function() {},
     onChange: null,
-    overlayBackgroundColor: 'rgba(0,0,0,0.7)'
 
 });
-// baguetteBox.run('.gallery', {options});
+
