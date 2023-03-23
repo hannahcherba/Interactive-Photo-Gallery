926cefe1e4ef9bd1bdf042169411df3d565f7eec
Hannah Cherba <hannahpond@yahoo.com>
2023-03-23 13:51:48 -0700

    created js search filter, added styling with css

--- photo_gallery_v5/js/app.js
+++ photo_gallery_v5/js/app.js
@@ -1,17 +1,41 @@
-const search = new Filter('search', 'data-caption');
+// const search = new Filter('search', 'data-caption');
 
-baguetteBox.run('.gallery', {
-    captions: true,
-    buttons: 'auto',
-    fullScreen: false,
-    noScrollbars: false,
-    titleTag: true,
-    async: false,
-    preload: 2,
-    // animation: 'fadeIn',
-    afterShow: function() {},
-    afterHide: function() {},
-    onChange: null,
+// baguetteBox.run('.gallery', {
+//     captions: true,
+//     buttons: 'auto',
+//     fullScreen: false,
+//     noScrollbars: false,
+//     titleTag: true,
+//     async: false,
+//     preload: 2,
+//     // animation: 'fadeIn',
+//     afterShow: function() {},
+//     afterHide: function() {},
+//     onChange: null,
+
+// });
+
+/*-----------------------------
+The JS Search
+------------------------------*/
+
+//Create an input variable
+
+const input = document.getElementById('search');
+
+//listen for search event
+
+input.addEventListener('keyup', e => {
+    let currentValue= e.target.value.toLowerCase();
+    let captions= document.querySelectorAll("p");
+    captions.forEach(caption => {
+        if (caption.textContent.toLowerCase().includes(currentValue)) {
+            caption.parentNode.parentNode.style.display = 'block';
+        }else {
+            caption.parentNode.parentNode.style.display = 'none';
+        }
+    });
 
 });
+
 
